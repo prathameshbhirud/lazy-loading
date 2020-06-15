7783dfdfdcdf7506a693381f1106004ab77c571f
Prathamesh <pratham_21289@yahoo.co.in>
2020-06-15 18:32:10 +0530

    angular lazy loading project added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'lazymodule', loadChildren: '../lazymodule/lazymodule.module#LazymoduleModule' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ src/app/lazymodule/lazymodule.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: Component1Component },
+  { path: 'component2', component: Component2Component },
+  { path: 'component3', component: Component3Component },
+];
+
+@NgModule({
+  declarations: [Component1Component, Component2Component, Component3Component],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class LazymoduleModule { }
